f3532d082ab686c1c48f804660995d89377ad531
mikemaruYT <scratchsun150@gmail.com>
2026-07-04 08:46:03 +0900

    1

--- texture-search/data.js
+++ texture-search/data.js
@@ -8,7 +8,8 @@ const jsonData = [
   {
     "id": "Coast-16x",//id wo awaseru
     "name": "Coast [16x]",
-    "link": "detail.html?id=Coast-16x", // 💡詳細ページへIDを引き渡すリンクに変更
+    "link": "detail.html?id=Coast-16x",
+    "icon": "Icons/notimage.png", // 💡詳細ページへIDを引き渡すリンクに変更
     "category": "1.8.9",
     "tags": ["16x", "Bridge","綺麗"],
     "content": `
@@ -21,6 +22,30 @@ const jsonData = [
         </ul>
         <h3>リンク</h3>
         <a href="https://www.youtube.com/watch?v=682B1TFbV2k" target="_blank">https://www.youtube.com/watch?v=682B1TFbV2k</a>
+        
+    `
+  },
+  {
+    "id": "無限",//id wo awaseru
+    "name": "無限pack",
+    "link": "detail.html?id=無限",
+    "icon": "Icons/notimage.png",  // 💡詳細ページへIDを引き渡すリンクに変更
+    "category": "1.8.9",
+    "tags": ["16x", "かっこいい","Skywars","UHC","Duel"],
+    "content": `
+        <h3>特徴</h3>
+        <ul>
+            <li>スカイウォーズ勢やUHC勢にとても人気なテクスチャです！<br>
+            これは知っている人多いんじゃないですか?<br>
+            全体的に赤って感じでダイヤ装備は黒ですね！<br>
+            めっちゃかっこよくて、skywarsとかUHCとかやってるひとには<br>
+            ぜひ使ってほしいテクスチャです！</li>
+            
+            <li><img src="images/無限.png" style="width: 40%;"><br>
+                引用:Forestさん</li>
+        </ul>
+        <h3>リンク</h3>
+        <a href="https://www.youtube.com/watch?v=daYZf2Sn6wg" target="_blank">https://www.youtube.com/watch?v=daYZf2Sn6wg</a>
         
     `
   }
