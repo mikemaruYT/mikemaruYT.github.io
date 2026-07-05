dfa5de95c8754addb64297d2a6896cced5af8765
mikemaruYT <scratchsun150@gmail.com>
2026-07-05 21:23:39 +0900

    1

--- texture-search/data.js
+++ texture-search/data.js
@@ -71,5 +71,75 @@ const jsonData = [
         <a href="https://www.youtube.com/watch?v=ZC0WkUZyyms" target="_blank">https://www.youtube.com/watch?v=ZC0WkUZyyms</a>
         
     `
+  },
+  {
+    "id": "Azura",//id wo awaseru
+    "name": "Azura 16x",
+    "link": "detail.html?id=Azura",  // 💡詳細ページへIDを引き渡すリンクに変更
+    "category": "1.8.9",
+    "image": "Icons/azura.png",
+    "tags": ["16x", "綺麗","Bedwars","Skywars","おしゃれ","Bridge","シンプル"],
+    "content": `
+        <h3>特徴</h3>
+        <ul>
+            <li>はいAzura16xきましたね～これ結構有名ですよ。<br>
+            雰囲気的にはCoast 16xとちょっと似てるんですけど、Coastより明るくて透明度あるテクスチャですね！<br>
+            Bridgeやってる人も結構使ってたりするし、bwのひとも使ってますね。<br>
+            剣は短めで羊毛は縁取りがあっていい感じですね！あと綺麗でおしゃれでめっちゃおすすめです。<br>
+            めっちゃみやすいし結構軽量化してくれるので万能なテクスチャですね！</li>
+            
+            <li><img src="images/Azura.png" style="width: 70%;"></li>
+        </ul>
+        <h3>リンク</h3>
+        <a href="https://www.youtube.com/watch?v=zLPUZbOxTsI" target="_blank">https://www.youtube.com/watch?v=zLPUZbOxTsI</a>
+        
+    `
+  },
+  {
+    "id": "Bedlessfault",//id wo awaseru
+    "name": "Bedlessfault 16x",
+    "link": "detail.html?id=Bedlessfault",  // 💡詳細ページへIDを引き渡すリンクに変更
+    "category": "1.8.9",
+    "image": "Icons/bedlessfault.png",
+    "tags": ["おしゃれ","かっこいい","16x","Bedwars","Bridge"],
+    "content": `
+        <h3>特徴</h3>
+        <ul>
+            <li>これは有名YoutuberのBedlessnoobさんのテクスチャですね。<br>
+            Bedlessnoobさんはブリッジとかをされている方なのでブリッジ勢は知っている人いるかもですね。<br>
+            このテクスチャ砂岩に水色の縁取りあったり、けっこうマイナーな羊毛の縁取りも刺さりますね。<br>
+            全体的に色が濃くてシンプル好きな人にはあんまり向かないかもしれませんが、好きな人はとことん好きだと思います。<br>
+            ベッドとかもオリジナルモデルがあってほんとに凝ったテクスチャだなって思いました。</li>
+            
+            <li><img src="images/Bedlessfault.png" style="width: 70%;"></li>
+            <li>引用:H4llowさん</li>
+        </ul>
+        <h3>リンク</h3>
+        <a href="https://www.youtube.com/watch?v=PHXyG4L9V9k" target="_blank">https://www.youtube.com/watch?v=PHXyG4L9V9k</a>
+        
+    `
+  },
+  {
+    "id": "Dewier20k",//id wo awaseru
+    "name": "Dewier 20k pack",
+    "link": "detail.html?id=Dewier20k",  // 💡詳細ページへIDを引き渡すリンクに変更
+    "category": "1.8.9",
+    "image": "Icons/dewier20k.png",
+    "tags": ["かっこいい","シンプル","Bedwars","RBW","16x"],
+    "content": `
+        <h3>特徴</h3>
+        <ul>
+            <li>きましたきました。これは我らがBedwars界隈の"神"であるDewierさんの20kテクスチャですね。<br>
+            この方DewierさんはRanked BedwarsをやっていらっしゃるだけあってこのテクスチャRBWでめっちゃ使いやすいです！<br>
+            いままでDewierさんがだしてきたテクスチャの中でスカイも剣も全部このテクスチャが一番好きです。<br>
+            Dewierさんのテクスチャでよく見るこの羊毛のテクスチャもほんとに見やすくて敵と戦いやすいですね。<br>
+            色も鮮やかで全体的にシンプルでそれでいてかっこよくて、16xなので軽量化にもなるというこれ以上のものがあるか?ってかんじのテクスチャですね(笑)</li>
+            
+            <li><img src="images/Dewier20k" style="width: 70%;"></li>
+        </ul>
+        <h3>リンク</h3>
+        <a href="https://www.youtube.com/watch?v=I-vjeyl2IhM" target="_blank">https://www.youtube.com/watch?v=I-vjeyl2IhM</a>
+        
+    `
   }
 ];
